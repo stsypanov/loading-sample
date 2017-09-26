ca25ed9837b06639ff2a94c69f1342634d8db09f
stsypanov <STsypanov@luxoft.com>
2017-09-26 11:09:23 +0300

    unify approach for getting class

--- src/main/java/com/tvs/Loader.java
+++ src/main/java/com/tvs/Loader.java
@@ -6,7 +6,7 @@ import java.io.InputStream;
 public class Loader {
 
     public int load() {
-        try (InputStream resourceAsStream = getClass().getClassLoader().getResourceAsStream("com/tvs/file1.txt")) {
+        try (InputStream resourceAsStream = Loader.class.getClassLoader().getResourceAsStream("com/tvs/file1.txt")) {
             return resourceAsStream.available();
         } catch (IOException e) {
             return 0;
